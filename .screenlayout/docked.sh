5440428c2ed7d43b67319672c7d68da20dab2b76
dspec12 <danspec12@gmail.com>
2019-07-20 17:30:46 -0400

    screen layout scripts

---
+++ .screenlayout/docked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS1 --off --output DP1 --off --output DP2 --primary --mode 3440x1440 --pos 0x1080 --rotate normal --output DP3 --mode 2560x1080 --pos 440x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
